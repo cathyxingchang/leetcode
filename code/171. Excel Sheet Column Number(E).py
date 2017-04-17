f868afb4a4fe7b941485bf14ffe8dc54bc2230f8
cathyxingchang <scse11061117@163.com>
2017-04-17 23:16:11 +0800

    171_ac

--- code/171. Excel Sheet Column Number(E).py
+++ code/171. Excel Sheet Column Number(E).py
@@ -3,10 +3,19 @@
 
 # 171. Excel Sheet Column Number(E)
 # Created by xc 17/04/2017
-
+"""
+    计算excel表格的数字 相当于是一个26进制 
+"""
 class Solution(object):
     def titleToNumber(self, s):
         """
         :type s: str
         :rtype: int
         """
+        result = 0
+        for item in s:
+            result = result * 26 + (ord(item) - ord('A') + 1)
+        return result
+s = "AZ"
+test = Solution()
+print test.titleToNumber(s)
